11012f266f3d15c4c599efbe06403ebc534d4694
noah1400 <48360809+noah1400@users.noreply.github.com>
2022-09-06 12:35:19 +0200

    Update SitemapXmlController.php

--- app/Http/Controllers/SitemapXmlController.php
+++ app/Http/Controllers/SitemapXmlController.php
@@ -9,10 +9,12 @@ use App\Models\Event;
 class SitemapXmlController extends Controller
 {
     public function index() {
-        $events = Event::all();
-        return SitemapGenerator::create('https://www.campusleben-es.de')
-            ->getSitemap()
-            ->add(Event::all())
-            ->toResponse(request());
+        $events = Event::all()->where('public', true);
+        $map= SitemapGenerator::create('https://www.campusleben-es.de')
+            ->getSitemap();
+        foreach ($events as $event) {
+            $map->add(url('/events/'.$event->id), $event->updated_at, '0.9', 'daily');
+        }
+        return $map->toResponse(request());
     }
 }
