daa004ada59036529b925bb15c615cd12c726c57
noah1400 <48360809+noah1400@users.noreply.github.com>
2022-09-18 22:36:10 +0200

    Removed Searchbar in dashboard

--- resources/js/components/layouts/dashboardLayout.vue
+++ resources/js/components/layouts/dashboardLayout.vue
@@ -171,35 +171,13 @@ export default {
             </div>
         </div>
         <div class="md:pl-64 flex flex-col">
-            <div class="sticky top-0 z-10 flex-shrink-0 flex h-16 bg-white shadow">
+            <div class="sticky top-0 z-10 flex-shrink-0 flex h-16 bg-transparent">
                 <button type="button"
-                    class="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
+                    class="px-4  text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
                     @click="sidebarOpen = true">
                     <span class="sr-only">Open sidebar</span>
                     <MenuAlt2Icon class="h-6 w-6" aria-hidden="true" />
                 </button>
-                <div class="flex-1 px-4 flex justify-between">
-                    <div class="flex-1 flex">
-                        <form class="w-full flex md:ml-0" action="#" method="GET">
-                            <label for="search-field" class="sr-only">Search</label>
-                            <div class="relative w-full text-gray-400 focus-within:text-gray-600">
-                                <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none">
-                                    <SearchIcon class="h-5 w-5" aria-hidden="true" />
-                                </div>
-                                <input id="search-field"
-                                    class="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
-                                    placeholder="Search" type="search" name="search" />
-                            </div>
-                        </form>
-                    </div>
-                    <div class="ml-4 flex items-center md:ml-6">
-                        <button type="button"
-                            class="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            <span class="sr-only">View notifications</span>
-                            <BellIcon class="h-6 w-6" aria-hidden="true" />
-                        </button>
-                    </div>
-                </div>
             </div>
 
             <main class="flex-1">
