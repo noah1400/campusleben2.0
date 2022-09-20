25de25875ed7cb4199195935effef94e9799ff85
noah1400 <48360809+noah1400@users.noreply.github.com>
2022-09-20 22:31:32 +0200

    Removed Account settings option

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -227,11 +227,6 @@
                                     <p class="text-sm font-medium text-gray-900 truncate" role="none">
                                         {{ Auth::user()->email }}</p>
                                 </div>
-                                <div class="py-1" role="none">
-                                    <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-                                    <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem"
-                                        tabindex="-1" id="menu-item-0">Account settings</a>
-                                </div>
                                 @admin
                                 <div class="py-1" role="none">
                                     <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
