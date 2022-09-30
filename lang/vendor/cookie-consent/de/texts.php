4e662f6bda400331711121b2c724669e6001f5f7
noah1400 <48360809+noah1400@users.noreply.github.com>
2022-09-30 21:22:04 +0200

    language cookioe consent

---
+++ lang/vendor/cookie-consent/de/texts.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'alert_accept' => 'Alle cookies akzeptieren',
+    'alert_essentials_only' => 'Nur notwendige Cookies akzeptieren',
+    'alert_settings' => 'Einstellungen anpassen',
+    'alert_text' => 'Durch die Auswahl von "Alle Cookies akzeptieren" stimmen Sie der Verwendung von Cookies zu, um Ihnen ein besseres Benutzererlebnis zu bieten und die Nutzung der Website zu analysieren. Durch Klicken auf "Einstellungen anpassen" können Sie auswählen, welche Cookies Sie zulassen möchten. Nur die notwendigen Cookies sind für den ordnungsgemäßen Betrieb unserer Website erforderlich und können nicht abgelehnt werden',
+    'alert_title' => 'Diese Website verwendet Cookies',
+    'alert_essential_only' => 'Nur notwendige Cookies akzeptieren',
+    'setting_analytics' => 'Analytische Cookies',
+    'setting_analytics_text' => 'erlauben uns die Nutzung der Website zu analysieren und das Erlebnis des Besuchers zu verbessern.',
+    'setting_essential' => 'Notwendige Cookies',
+    'setting_essential_text' => 'sind aus technischen Gründen erforderlich. Ohne sie funktioniert diese Website möglicherweise nicht ordnungsgemäß.',
+    'setting_functional' => 'Funktionale Cookies',
+    'setting_functional_text' => 'sind für bestimmte Funktionen auf der Website erforderlich. Ohne sie können einige Funktionen deaktiviert werden.',
+    'setting_marketing' => 'Marketing-Cookies',
+    'setting_marketing_text' => 'erlauben es uns, Ihre Erfahrung zu personalisieren und Ihnen relevante Inhalte und Angebote, auf dieser Website und anderen Websites, zu senden.',
+    'settings_accept_all' => 'Alle Cookies akzeptieren',
+    'settings_cancel' => 'Abbrechen',
+    'settings_close' => 'Schließen',
+    'settings_save' => 'Meine Auswahl speichern',
+    'settings_title' => 'Cookie-Einstellungen',
+    'settings_text' => 'Unsere Website speichert vier Arten von Cookies. Sie können jederzeit auswählen, welche Cookies Sie akzeptieren und welche Sie ablehnen möchten. Sie können mehr darüber lesen, was Cookies sind und welche Arten von Cookies wir in unserer <a href=":policyUrl" target="_blank" class="underline hover:no-underline">Cookie-Richtlinie</a> speichern.'
+    ];
