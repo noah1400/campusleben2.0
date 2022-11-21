ebf5c519f809b3ecf8a0e97c73c42741a648650e
noah1400 <48360809+noah1400@users.noreply.github.com>
2022-11-21 15:55:07 +0100

    Add  Locations to sitemap

--- app/Http/Controllers/SitemapXmlController.php
+++ app/Http/Controllers/SitemapXmlController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Spatie\Sitemap\SitemapGenerator;
 use App\Models\Event;
+use App\Models\Location;
 
 class SitemapXmlController extends Controller
 {
@@ -14,6 +15,11 @@ class SitemapXmlController extends Controller
             ->getSitemap();
         foreach ($events as $event) {
             $map->add(url('/events/'.$event->id), $event->updated_at, '0.9', 'daily');
+        }
+
+        $locations = Location::all()->where('clickable', true);
+        foreach ($locations as $location) {
+            $map->add(url('/l/'.$location->slug), $location->updated_at, '0.9', 'daily');
         }
         return $map->toResponse(request());
     }
