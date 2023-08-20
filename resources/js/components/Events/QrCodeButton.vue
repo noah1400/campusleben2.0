17a3a8221e8e87b93157a5994e13a5ded0adaa08
noah1400 <48360809+noah1400@users.noreply.github.com>
2023-08-20 14:07:49 +0200

    Update QrCodeButton.vue

--- resources/js/components/Events/QrCodeButton.vue
+++ resources/js/components/Events/QrCodeButton.vue
@@ -117,7 +117,7 @@ export default {
 
             const pdfOptions = {
                 margin: 10,
-                filename: this.event.name + '-qr-code.pdf',
+                filename: this.event.name.replaceAll(' ', '-') + '-qr-code.pdf',
                 image: { type: 'jpeg', quality: 0.98 },
                 html2canvas: { scale: 2 },
                 jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
