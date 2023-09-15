22708e1b564d26c05b67ab38eedffc8281f2231a
Noah Scholz <48360809+noah1400@users.noreply.github.com>
2023-09-15 16:27:36 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
             <div class="text-center">
                 <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Willkommen!</h2>
                 <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Hier findet ihr alles was das
-                    Studentenherz begehrt.</h2>
+                    Studierendenherz begehrt.</h2>
                 <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl"> #Campuswillleben</h2>
             </div>
             <div class="max-w-lg mx-auto p-5">
